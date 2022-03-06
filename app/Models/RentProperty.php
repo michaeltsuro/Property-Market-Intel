885f0b287d55d55e95550886fd05ba2e3b94d74e
michaeltsuro <michaeltsuro@gmail.com>
2022-03-06 16:12:55 +0200

    Backend entities

---
+++ app/Models/RentProperty.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RentProperty extends Model
+{
+    use HasFactory;
+}
